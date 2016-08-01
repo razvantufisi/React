10ab3844df548c4c5a7e00466d545d5d138e61d4
razvantufisi <razvantufisi94@yahoo.ro>
2016-08-01 13:23:14 +0300

    Doar steup

---
+++ src/components/app.js
@@ -0,0 +1,22 @@
+/*eslint-disable strict */ //Disabling check because we can't run strict mode. Need global vars.
+
+var React = require('react');
+var RouteHandler = require('react-router').RouteHandler;
+$ = jQuery = require('jquery');
+
+var App = React.createClass({
+	render: function() {
+		return (
+			<div>
+				
+				<div className="container-fluid">
+				
+				Here IS Me
+					</div>
+				
+			</div>
+		);
+	}
+});
+
+module.exports = App;
---
+++ src/routes.js
@@ -0,0 +1,16 @@
+"use strict";
+
+var React = require('react');
+
+var Router = require('react-router');
+var DefaultRoute = Router.DefaultRoute;
+var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
+var Redirect = Router.Redirect;
+
+var routes = (
+  <Route name="app" path="/" handler={require('./components/app')}>
+  </Route>
+);
+
+module.exports = routes;
